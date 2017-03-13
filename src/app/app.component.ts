759b20c0f348a91ed9c109111ee8b8780b13b1b6
Jerry <jerry@miniasp.com>
2017-03-13 12:29:43 +0800

    任務 06：套用事件繫結 (Event Binding) 任務 07：套用雙向繫結 (Two-way Binding)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   inputHint  = 'What needs to be done?';
+  todos: any[] = [];
+  todo = '';
+
+  addTodo() {
+    this.todos.push(this.todo);
+    this.todo = '';
+  }
 }
