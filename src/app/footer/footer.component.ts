2bb9fe2c6c24d9225c92daf23d8ee528753f9b88
Jerry Lin <jerry.jzlin@gmail.com>
2017-03-25 23:03:18 +0800

    任務 10 進階練習：實作 ngOnChanges 方法。 當未完成數量超過 5 筆資料時，'item left' 顯示為紅色

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnChanges {
 
   @Input()
   todos: any[];
@@ -18,9 +18,15 @@ export class FooterComponent implements OnInit {
   @Output()
   filterTypeChange = new EventEmitter<string>();
 
+  tooMuch = false;
+
   constructor () { }
 
   ngOnInit () {
+  }
+
+  ngOnChanges () {
+    this.tooMuch = this.todos.filter(item => !item.done).length > 5;
   }
 
   clearBtnClick () {
